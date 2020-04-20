ee15a75eec10031f45f90849f9526d7a4ff12075
= <=>
2020-04-19 19:32:11 -0700

    Add counter action

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -3,3 +3,21 @@ const DESCREASE = 'counter/DECREASE';
 
 export const increase = ()=>({type:INCREASE});
 export const decrease = ()=>({type:DESCREASE});
+
+const initialState = {
+    number: 0
+}
+
+function counter(state =initialState, action){
+    switch(action.type)
+    {
+        case INCREASE:
+            return {number:state.number+1};
+        case DESCREASE:
+            return {number:state.number-1};
+        default:
+            return state;
+    }
+}
+
+export default counter;
