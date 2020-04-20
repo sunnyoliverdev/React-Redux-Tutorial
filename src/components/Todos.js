159698efbf62c63666841f276269a23c7cf0d5e6
= <=>
2020-04-19 19:25:44 -0700

    Add Todo component

---
+++ src/components/Todos.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const TodoItem = ({todo, onToggle, onRemove}) =>{
+    return (
+        <div>
+            <input type='checkbox'/>
+            <span>Example text</span>
+            <button>Remove</button>
+        </div>
+    );
+};
+
+const Todos = ({
+    input,
+    todos,
+    onChange,
+    onInsert,
+    onToggle,
+    onRemove
+})=>{
+    const onSubmit = e =>{
+        e.preventDefault();
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input />
+                <button type='submit'>Register</button>
+            </form>
+            <div>
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </div>
+        </div>
+    );
+};
